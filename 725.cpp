7ccfe22b819b3a104e10f26fa4a18c55df71c516
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-09-08 22:46:26 +0530

    Create 725.cpp
    Splits Linked LIst into parts

---
+++ 725.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        // 1 find the length;
+        ListNode* curr = head;
+        int L = 0;
+        while(curr){
+            L++;
+            curr = curr->next;
+        }
+
+        int eachbucketnode = L/k;
+        int remaindernode = L%k;
+
+        vector<ListNode*> result(k,NULL);
+        curr = head;
+        ListNode* prev = NULL;
+        
+        for(int i = 0;i<k;i++){
+            result[i] = curr;
+            for(int count = 1;count <= eachbucketnode + (remaindernode > 0 ? 1:0);count++){
+                prev = curr;
+                curr = curr->next;
+            }
+            if(prev != NULL) prev->next = NULL;
+            remaindernode--;
+        }
+        return result;
+    }
+};

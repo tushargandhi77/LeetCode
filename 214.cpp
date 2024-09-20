d6fe7ab812ba6a1e238b9c43eee224067918c8bf
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-09-20 22:50:17 +0530

    Create 214.cpp

---
+++ 214.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(begin(rev),end(rev));
+
+        for(int i = 0;i < s.length();i++){
+            if(memcmp(s.c_str(),rev.c_str() + i,s.length()-i) == 0){
+                return rev.substr(0,i) + s;
+            }
+        }
+        return rev + s;
+    }
+};

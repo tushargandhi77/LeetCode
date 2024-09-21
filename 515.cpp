b8be1b35686c9a3d8a8b40dcc8f805e9038a4018
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-09-21 15:02:09 +0530

    Create 515.cpp

---
+++ 515.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root) return {};
+        vector<int> result;
+        queue<TreeNode*> que;
+        que.push(root);
+        // result.push_back(root->val);
+        while(!que.empty()){
+            int n = que.size();
+            int maxVal = INT_MIN;
+            while(n--){
+                TreeNode* node = que.front();
+                que.pop();
+                maxVal = max(maxVal,node->val);
+
+                if(node->left) que.push(node->left);
+                if(node->right) que.push(node->right);
+            }
+            result.push_back(maxVal);
+        }
+        return result;
+    }
+};

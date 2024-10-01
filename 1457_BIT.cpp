44e6c3b174c8f1dc087daff192cc1ed1b401ab1d
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-10-01 21:01:38 +0530

    Create 1457_BIT.cpp

---
+++ 1457_BIT.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int result = 0;
+    void solve(TreeNode* root,int val){
+        if(!root) return;
+
+        val = (val ^ (1<<root->val));
+        if(root->left == NULL && root->right == NULL){
+            if((val & (val-1))==0) result++;
+        }
+        solve(root->left,val);
+        solve(root->right,val);
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        int val = 0;
+        solve(root,val);
+        return result;
+    }
+};

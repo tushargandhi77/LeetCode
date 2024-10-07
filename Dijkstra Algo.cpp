8ced6ca92a42db1bbcbb698106ddb92acc5411d2
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-10-07 20:47:31 +0530

    Create Dijkstra Algo.cpp

---
+++ Dijkstra Algo.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int source)
+    {
+        // Code here
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        
+        vector<int> result(V,INT_MAX);
+        
+        result[source] = 0;
+        pq.push({0,source});
+        
+        
+        
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            for(auto& vec:adj[node]){
+                int adjNode = vec[0];
+                int dist = vec[1];
+                
+                if(d + dist < result[adjNode]){
+                    result[adjNode] = d+dist;
+                    pq.push({d+dist,adjNode});
+                }
+                
+            }
+        }
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends

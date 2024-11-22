baee691f2ef0e3289b5d0115fa3cc9cec1bb953c
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-11-22 13:04:11 +0530

    Create 1072.cpp

---
+++ 1072.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        int m =matrix.size();
+        int n = matrix[0].size();
+
+        int maxRow = 0;
+
+        for(auto& currRow: matrix){
+            vector<int> inverted(n);
+
+            for(int i = 0;i<n;i++){
+                inverted[i] = 1 - currRow[i];
+            }
+
+            int count= 0;
+
+            for(auto& row: matrix){
+                if(row == currRow || row == inverted){
+                    count++;
+                }
+            }
+            maxRow = max(count,maxRow);
+        }
+        return maxRow;
+    }
+};

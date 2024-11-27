809708e51461f0176aa62c0bfeff084f89f67289
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-11-27 16:57:48 +0530

    Create Flattening a linked list.cpp

---
+++ Flattening a linked list.cpp
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *bottom;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+
+void printList(Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // Function which returns the  root of the flattened linked list.
+    Node *mergetwolist(Node* head1,Node* head2){
+        if(head1 == NULL) return head2;
+        if(head2 == NULL) return head1;
+        
+        Node* result;
+        if(head1->data < head2->data){
+            result = head1;
+            result->bottom = mergetwolist(head1->bottom,head2);
+        }
+        else{
+            result = head2;
+            result->bottom = mergetwolist(head1,head2->bottom);
+        }
+        return result;
+    }
+    Node *flatten(Node *root) {
+        // Your code here
+        if(root==NULL || root->next == NULL) return root;
+        
+        Node* head2 = flatten(root->next);
+        
+        root = mergetwolist(root,head2);
+        
+        return root;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        vector<int> work;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            work.push_back(number);
+        }
+        n = work.size();
+
+        Node *head = NULL;
+        Node *pre = NULL;
+
+        for (int i = 0; i < n; i++) {
+            int m = work[i] - 1;
+            int data;
+            cin >> data;
+            Node *temp = new Node(data);
+            if (head == NULL) {
+                head = temp;
+                pre = temp;
+            } else {
+                pre->next = temp;
+                pre = temp;
+            }
+
+            Node *preB = temp;
+            for (int j = 0; j < m; j++) {
+                int temp_data;
+                cin >> temp_data;
+                Node *tempB = new Node(temp_data);
+                preB->bottom = tempB;
+                preB = tempB;
+            }
+        }
+
+        Solution ob;
+        Node *root = ob.flatten(head);
+        printList(root);
+    }
+    return 0;
+}
+
+// } Driver Code Ends

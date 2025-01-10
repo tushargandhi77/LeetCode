234181e600f6375ee670ac05cd04aa795a8e91ee
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-01-10 20:26:11 +0530

    Create 916.cpp

---
+++ 916.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isvalid(vector<int>& freq2,vector<int>& temp){
+        for(int i = 0;i<26;i++){
+            if(temp[i] < freq2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> result;
+
+        vector<int> freq2(26,0);
+
+        for(string& word: words2){
+            vector<int> temp(26,0);
+
+            for(char& ch: word){
+                temp[ch-'a']++;
+
+                freq2[ch-'a'] = max(freq2[ch-'a'],temp[ch-'a']);
+            }
+        }
+
+        for(string& word: words1){
+            vector<int> temp(26,0);
+
+            for(char& ch: word){
+                temp[ch-'a']++;
+            }
+
+            if(isvalid(freq2,temp)==true) result.push_back(word);
+        }
+        return result;
+    }
+};

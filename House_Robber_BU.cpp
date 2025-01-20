8c89751469f45a0b424074310976a04b794aa3bb
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-01-20 18:50:59 +0530

    Create House_Robber_BU.cpp

---
+++ House_Robber_BU.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> t(n+1);
+        t[0] = 0;
+        t[1] = nums[0];
+
+        for(int i = 2;i<=n;i++){
+            t[i] = max(nums[i-1] + t[i-2],t[i-1]);
+        }
+
+        return t[n];
+    }
+};

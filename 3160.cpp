b9ea9c29b87d7c197f2e15b7844fe825c078490a
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-02-07 15:08:33 +0530

    Create 3160.cpp

---
+++ 3160.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        int n = queries.size();
+        vector<int> result(n);
+
+        unordered_map<int,int> colorMp;
+
+
+        unordered_map<int,int> ballMp;
+
+        for(int i = 0;i<n;i++){
+            int ball = queries[i][0];
+            int color = queries[i][1];
+
+            if(ballMp.count(ball)){
+                int prevColor = ballMp[ball];
+                colorMp[prevColor]--;
+
+                if(colorMp[prevColor] == 0){
+                    colorMp.erase(prevColor);
+                }
+            }
+
+            ballMp[ball] = color;
+            colorMp[color]++;
+
+            result[i] = colorMp.size();
+        }
+        return result;
+    }
+};

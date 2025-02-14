0a74ef793e35b900d2464b4429af052a9f7a5e3a
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-02-14 14:51:07 +0530

    Create 1352_BF.cpp

---
+++ 1352_BF.cpp
@@ -0,0 +1,27 @@
+class ProductOfNumbers {
+public:
+    vector<int> nums;
+    ProductOfNumbers() {
+        nums.clear();
+    }
+    
+    void add(int num) {
+        nums.push_back(num);
+    }
+    
+    int getProduct(int k) {
+        int n = nums.size();
+        int product = 1;
+        for(int i = n-k;i<n;i++){
+            product *= nums[i];
+        }
+        return product;
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */

1e27e4368963fb10e2fff7e027f8d046c6f903d7
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-03-02 12:30:04 +0530

    Create 2570.cpp

---
+++ 2570.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        unordered_map<int,int> mp;
+
+        for(auto& num: nums1){
+            int idx = num[0];
+            int val = num[1];
+
+            mp[idx] = val;
+        }
+        for(auto& num: nums2){
+            int idx = num[0];
+            int val = num[1];
+
+            mp[idx] += val;
+        }
+
+        vector<vector<int>> result;
+
+        for(auto& it : mp){
+            result.push_back({it.first,it.second});
+        }
+        sort(begin(result),end(result));
+        return result;
+    }
+};

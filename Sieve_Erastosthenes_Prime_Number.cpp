e9dd4368a6c3e150e1b6b26d80110cdc5c595673
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-03-07 15:39:31 +0530

    Update Sieve_Erastosthenes_Prime_Number.cpp

--- Sieve_Erastosthenes_Prime_Number.cpp
+++ Sieve_Erastosthenes_Prime_Number.cpp
@@ -9,9 +9,12 @@ vector<bool> isPrime(n+1,true);
                 int i = 2;
 
                 while(idx*i <= n){
-                    isPrime[i*idx] = false;
+                    isPrime[i*idx] = false; // // n/2 + n/3 + n/7 + ....  harmonic serires on  primes very slow related to log(n) 
                     i++;
                 }
             }
             idx++;
         }
+
+// n(1/2 + 1/3 + ... )
+// TC - O( n * log(log(n))

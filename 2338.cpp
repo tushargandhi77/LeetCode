bc4d17f5afb155642615501c2c8c5d6f32e950e2
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-04-23 17:24:22 +0530

    Update 2338.cpp

--- 2338.cpp
+++ 2338.cpp
@@ -1,51 +1,66 @@
-const int MOD = 1e9 + 7, MAX_N = 1e4 + 10,
-          MAX_P = 15;  // There are up to 15 prime factors
-int c[MAX_N + MAX_P][MAX_P + 1];
-vector<int> ps[MAX_N];  // List of prime factor counts
-int sieve[MAX_N];       // Minimum prime factor
-
 class Solution {
 public:
-    Solution() {
-        if (c[0][0]) {
-            return;
+    int M = 1e9+7;
+    int findPower(long long a , long long b){
+        if( b == 0) return 1;
+
+        long long half = findPower(a,b/2);
+        long long result = (half*half)%M;
+        if(b%2 == 1){
+            result = (result * a) % M;
         }
-        for (int i = 2; i < MAX_N; i++) {
-            if (sieve[i] == 0) {
-                for (int j = i; j < MAX_N; j += i) {
-                    sieve[j] = i;
+
+        return result;
+    }
+    void findSets(int m,vector<int>& count,vector<vector<int>>& dp){
+        if(dp[m][1] != 0) return;
+
+        dp[m][1] = 1;
+        count[1]++;
+
+        for(int div = 2;div<=m;div++){
+            if(m % div == 0){
+                findSets(m/div,count,dp);
+
+                for(int len = 1;len < 15;len++){
+                    if(dp[m/div][len] != 0){
+                        dp[m][len+1] += dp[m/div][len];
+                        count[len+1] += dp[m/div][len];
+                    }
                 }
-            }
-        }
-        for (int i = 2; i < MAX_N; i++) {
-            int x = i;
-            while (x > 1) {
-                int p = sieve[x];
-                int cnt = 0;
-                while (x % p == 0) {
-                    x /= p;
-                    cnt++;
-                }
-                ps[i].push_back(cnt);
-            }
-        }
-        c[0][0] = 1;
-        for (int i = 1; i < MAX_N + MAX_P; i++) {
-            c[i][0] = 1;
-            for (int j = 1; j <= min(i, MAX_P); j++) {
-                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % MOD;
             }
         }
     }
-    int idealArrays(int n, int maxValue) {
-        long long ans = 0;
-        for (int x = 1; x <= maxValue; x++) {
-            long long mul = 1;
-            for (int p : ps[x]) {
-                mul = mul * c[n + p - 1][p] % MOD;
-            }
-            ans = (ans + mul) % MOD;
+
+    int modularnCr(int n,int r,vector<long long>& fact){
+        if( r < 0 || r > n) return 0;
+
+        long long b = (fact[r] * fact[n-r]) % M;
+        return (fact[n] * findPower(b,M-2)) % M;
+    }
+    int idealArrays(int n, int maxVal) {
+        vector<vector<int>> dp(maxVal+1,vector<int>(15,0));
+        vector<int> count(15,0);
+
+        for(int val = 1;val<=maxVal;val++){
+            findSets(val,count,dp);
+        }   
+        vector<long long> fact(n+1,1);
+        for(int i = 2;i<=n;i++){
+            fact[i] = (fact[i-1]*i)%M;
         }
-        return ans;
+
+        long long result = 0;
+        for(int len = 1;len < 15;len++){
+            if( n < len){
+                break;
+            }
+            if(count[len] != 0 ){
+            long long possibilities = modularnCr(n-1,len-1,fact);
+            result = (result + (count[len] * possibilities)%M)%M;
+            }
+        }
+
+        return (int)result;
     }
 };

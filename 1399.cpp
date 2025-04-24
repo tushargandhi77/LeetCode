47bafeca518d010df42622efc324e2dafd273f43
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-04-24 12:59:39 +0530

    Update 1399.cpp

--- 1399.cpp
+++ 1399.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
+    int countsum(int n){
+        int sum = 0;
+        while(n > 0){
+            sum += n % 10;
+            n/=10;
+        }
+
+        return sum;
+    }
     int countLargestGroup(int n) {
         unordered_map<int,int> mp;
+        int count = 0;
+        int maxVal = 0;
 
         for(int i = 1;i<=n;i++){
-            int num = i;
-            int sum = 0;
-            while(num > 0){
-                int d = num % 10;
-                sum += d;
-                num /= 10;
-            }
+            int sum = countsum(i);
             mp[sum]++;
-        }
-
-        int max = 0;
-        int count = 0;
-        for(auto it: mp){
-            if(it.second > max){
-                max = it.second;
+            
+            if(mp[sum] == maxVal){
+                count++;
+            }
+            else if(mp[sum] > maxVal){
+                maxVal = mp[sum];
+                count = 0;
             }
         }
 
-        for(auto it: mp){
-            if(it.second == max) count++;
-        }
-
-        return count;
+        return count+1;
     }
 };

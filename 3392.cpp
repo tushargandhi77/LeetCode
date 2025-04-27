0eaad005423182688fed1b846078f4f8eaefc238
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-04-27 17:04:03 +0530

    Create 3392.cpp

---
+++ 3392.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for(int i = 0;i<n-2;i++){
+            if((nums[i]+nums[i+2])*2 == nums[i+1]) count++;
+        }
+
+        return count;
+    }
+};

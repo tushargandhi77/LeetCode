aaedbba638a05293f312258a717cf244b1eabb9d
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-05 16:02:16 +0530

    Create 72_BU.cpp

---
+++ 72_BU.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        vector<vector<int>> t(m+1,vector<int>(n+1,0));
+
+        for(int i = 0;i<=m;i++){
+            for(int j = 0;j<=n;j++){
+                if(i == 0 || j == 0){
+                    t[i][j] = i + j;
+                }
+                else if(s1[i-1] == s2[j-1]){
+                    t[i][j] = t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = min({1 + t[i-1][j],1 + t[i][j-1],1 + t[i-1][j-1]});
+                }
+            }
+        }
+
+        return t[m][n];
+    }
+};

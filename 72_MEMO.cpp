6c1fe70cfad0f55e37041ced7a4162053fdd3f2e
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-05 15:56:48 +0530

    Update 72_MEMO.cpp

--- 72_MEMO.cpp
+++ 72_MEMO.cpp
@@ -30,3 +30,36 @@ public:
         return solve(0,0,word1,word2);
     }
 };
+
+
+// Method 2
+
+class Solution {
+public:
+    int t[501][501];
+    int solve(string& s1,string& s2,int m,int n){
+        if(m == 0 || n == 0) return m+n;
+
+        if(t[m][n] != -1) return t[m][n];
+
+        if(s1[m-1] == s2[n-1]){
+            return t[m][n] = solve(s1,s2,m-1,n-1);
+        }
+        else {
+            int insertC = 1 + solve(s1,s2,m,n-1);
+            int deleteC = 1 + solve(s1,s2,m-1,n);
+            int replaceC = 1 + solve(s1,s2,m-1,n-1);
+
+            return t[m][n] = min({insertC,deleteC,replaceC});
+        }
+
+        return -1;
+    }
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        memset(t,-1,sizeof(t));
+
+        return solve(word1,word2,m,n);
+    }
+};

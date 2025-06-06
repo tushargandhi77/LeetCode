47421b08c3ca8c7fccac03ce2f2cc21575c2bb3c
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-06 15:31:25 +0530

    Update 647_Palindromic_Substring.cpp

--- 647_Palindromic_Substring.cpp
+++ 647_Palindromic_Substring.cpp
@@ -26,3 +26,36 @@ public:
     }
 };
 
+
+// Method 2 :- Dynamtic Programminc Memoization
+
+class Solution {
+public:
+    int t[1001][1001];
+    bool check(int i,int j,string& s){
+        if(i > j) return true;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        if(s[i] == s[j]){
+            return t[i][j] = check(i+1,j-1,s);
+        }
+
+        return t[i][j] = false;
+    }
+    int countSubstrings(string& s) {
+        int n = s.length();
+        int count = 0;
+
+        memset(t,-1,sizeof(t));
+
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                if(check(i,j,s)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};

4b90b4df9da397724ba92f4921980af0d44b42bb
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-07 12:16:41 +0530

    Update 516.cpp

--- 516.cpp
+++ 516.cpp
@@ -23,3 +23,29 @@ public:
         return LCS(s1,s,n,n);
     }
 };
+
+
+// Method 2 : Recusion and Memo
+
+class Solution {
+public:
+    int t[1001][1001];
+    int solve(int i,int j,string& s){
+        if(i > j) return 0;
+
+        if(i == j) return 1;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        if(s[i] == s[j]){
+            return t[i][j] = 2 + solve(i+1,j-1,s);
+        }
+
+        return t[i][j] = max(solve(i+1,j,s),solve(i,j-1,s));
+    }
+    int longestPalindromeSubseq(string s) {
+        memset(t,-1,sizeof(t));
+        int n = s.length();
+        return solve(0,n-1,s);
+    }
+};

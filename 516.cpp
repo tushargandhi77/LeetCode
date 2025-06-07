fb7cf51db083081fd1517fb498e6015b11956e81
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-07 12:33:06 +0530

    Update 516.cpp

--- 516.cpp
+++ 516.cpp
@@ -24,6 +24,34 @@ public:
     }
 };
 
+// Method 2 :- Bottom Up LCS
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+
+        string s1 = s;
+        reverse(begin(s),end(s));
+        string s2 = s;
+
+        vector<vector<int>> t(n+1,vector<int>(n+1,0));
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                if(s1[i-1] == s2[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+
+        return t[n][n];
+    }
+};
+
 
 // Method 2 : Recusion and Memo
 

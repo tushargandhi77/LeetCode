4d3a36b1536ba174fe2cb600c727e0c04cc82680
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-08 13:08:32 +0530

    Update 1312.cpp

--- 1312.cpp
+++ 1312.cpp
@@ -22,3 +22,30 @@ public:
         return solve(0,n-1,s);
     }
 };
+
+
+// Method 2: - Bottom Up
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.length();
+        
+        vector<vector<int>> t(n,vector<int>(n,0));
+
+        for(int L = 2;L<=n;L++){
+            for(int i = 0;i<(n-L+1);i++){
+                int j = i + L -1;
+
+                if(s[i] == s[j]){
+                    t[i][j] = t[i+1][j-1];
+                }
+                else{
+                    t[i][j] = 1 + min(t[i+1][j],t[i][j-1]);
+                }
+            }
+        }
+
+        return t[0][n-1];
+    }
+};

e890ccc384d41e02614ad61dbc5032bc885a77c7
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-10 14:49:24 +0530

    Update 132.cpp

--- 132.cpp
+++ 132.cpp
@@ -82,3 +82,53 @@ public:
         return solve(0,n-1,s);
     }
 };
+
+
+// Method 3 : 
+
+class Solution {
+public:
+    int t[2001][2001];
+    int solve(int i,int j,string& s,vector<vector<bool>>& d){
+        if(i >= j) return 0;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        if(d[i][j]){
+            return 0;
+        }
+
+
+        int cuts = INT_MAX;
+
+        for(int k = i;k<=j-1;k++){
+            int temp = 1 + solve(i,k,s,d) + solve(k+1,j,s,d);
+            cuts = min(cuts,temp);
+        }
+
+        return t[i][j] = cuts;
+    }
+    int minCut(string s) {
+        int n = s.length();
+        memset(t,-1,sizeof(t));
+        
+        vector<vector<bool>> d(n,vector<bool>(n,false));
+
+        for(int L = 1;L<=n;L++){
+            for(int  i = 0;i<(n-L+1);i++){
+                int j = i + L -1;
+    
+                if(i == j){
+                    d[i][i] = true;
+                }
+                else if(i+1 == j){
+                    d[i][j] = (s[i] == s[j]);
+                }
+                else{
+                    d[i][j] = ((s[i] == s[j]) && d[i+1][j-1]);
+                }
+            }
+        }
+        return solve(0,n-1,s,d);
+    }
+};

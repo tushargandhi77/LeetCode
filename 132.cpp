fb989568e7207a574c4be7af8b8fe227e16df612
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-10 14:54:12 +0530

    Update 132.cpp

--- 132.cpp
+++ 132.cpp
@@ -132,3 +132,47 @@ public:
         return solve(0,n-1,s,d);
     }
 };
+
+
+// Method 4 
+
+class Solution {
+public:
+    int minCut(string s) {
+        int n = s.length();
+        vector<vector<bool>> t(n,vector<bool>(n,false));
+
+        for(int L = 1;L<=n;L++){
+            for(int i = 0;i<(n-L+1);i++){
+                int j = i + L -1;
+                if(i==j){
+                    t[i][i] = true;
+                }
+                else if(i+1 == j){
+                    t[i][j] = s[i] == s[j];
+                }
+                else{
+                    t[i][j] = ((s[i] == s[j]) && t[i+1][j-1]);
+                }
+
+            }
+        }
+
+        vector<int> dp(n);
+        for(int i = 0;i<n;i++){
+            if(t[0][i] == true){
+                dp[i] = 0;
+            }
+            else{
+                dp[i] = INT_MAX;
+                for(int k = 0;k<i;k++){
+                    if(t[k+1][i] == true && 1 + dp[k] < dp[i]){
+                        dp[i] = 1 + dp[k];
+                    }
+                }
+            }
+        }
+
+        return dp[n-1];
+    }
+};

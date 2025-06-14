c8fdc536420ae9bf42f9e34b3955bc297aae752c
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-14 14:13:25 +0530

    Update 62_MEMO.cpp

--- 62_MEMO.cpp
+++ 62_MEMO.cpp
@@ -13,4 +13,23 @@ public:
         memset(t,-1,sizeof(t));
         return solve(0,0,m,n);
     }
+}; 
+
+// Recusion 
+
+class Solution {
+public:
+    int solve(int i,int j,int m,int n){
+        if(i == m-1 && j == n-1) return 1;
+
+        if(i < 0 || i >= m || j < 0 || j >= n) return 0;
+
+        int right = solve(i+1,j,m,n);
+        int bottom = solve(i,j+1,m,n);
+
+        return right + bottom;
+    }
+    int uniquePaths(int m, int n) {
+        return solve(0,0,m,n);
+    }
 };

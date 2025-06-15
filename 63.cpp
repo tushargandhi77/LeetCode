4b588f7292132167d5003ae0ebfb1e56a8ff5efb
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-15 11:42:41 +0530

    Update 63.cpp

--- 63.cpp
+++ 63.cpp
@@ -24,3 +24,33 @@ public:
         return solve(0,0,Grid);
     }
 };
+
+// Method 2 :-> Memoization
+
+class Solution {
+public:
+    int m,n;
+    int t[101][101];
+    int solve(int i,int j,vector<vector<int>>& Grid){
+        if(i < 0 || i >= m || j < 0 || j >= n || Grid[i][j] == 1){
+            return 0;
+        }
+        
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+
+        if(t[i][j] != -1) return t[i][j];
+
+        int right = solve(i,j+1,Grid);
+        int down = solve(i+1,j,Grid);
+
+        return t[i][j] = right+down;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+        m = Grid.size();
+        n = Grid[0].size();
+        memset(t,-1,sizeof(t));
+        return solve(0,0,Grid);
+    }
+};

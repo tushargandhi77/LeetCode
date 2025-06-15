ba89c52cf27b7b67bb0b67d1c65f67c7583118a9
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-15 11:47:55 +0530

    Update 63.cpp

--- 63.cpp
+++ 63.cpp
@@ -54,3 +54,55 @@ public:
         return solve(0,0,Grid);
     }
 };
+
+// Method 3 :-> Botom Up
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+        int m = Grid.size();
+        int n = Grid[0].size();
+
+        vector<vector<int>> t(m,vector<int>(n));
+
+        for(int col = 0;col<n;col++){
+
+            if(Grid[0][col] == 1){
+                t[0][col] = 0;
+            }
+            else if(col > 0 && Grid[0][col-1] == 1){
+                t[0][col] = 0;
+                Grid[0][col] = 1;
+            }
+            else{
+                t[0][col] = 1;
+            }
+        }
+
+        for(int row = 0;row<m;row++){
+            if(Grid[row][0] == 1){
+                t[row][0] = 0;
+            }
+            else if(row > 0 && Grid[row-1][0] == 1){
+                t[row][0] = 0;
+                Grid[row][0] = 1;
+            }
+            else{
+                t[row][0] = 1;
+            }
+        }
+
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                if(Grid[i][j] == 1){
+                    t[i][j] = 0;
+                }
+                else{
+                    t[i][j] = t[i-1][j] + t[i][j-1];
+                }
+            }
+        }
+
+        return t[m-1][n-1];
+    }
+};

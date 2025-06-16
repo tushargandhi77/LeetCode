01d4c0965cad0c1d1bd4dbec3ba0f6a51381a024
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-16 15:35:50 +0530

    Update 1594.cpp

--- 1594.cpp
+++ 1594.cpp
@@ -42,3 +42,43 @@ public:
         return MaxProd < 0 ? -1 : MaxProd % MOD;    
     }
 };
+
+
+// Method 2 -> bottom UP
+
+class Solution {
+public:
+    int MOD = 1e9+7;
+    typedef long long ll;
+    int maxProductPath(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<pair<ll,ll>>> t(m,vector<pair<ll,ll>>(n));
+
+        t[0][0] = make_pair(grid[0][0],grid[0][0]);
+
+        // BASE CASE
+        for(int col=1;col<n;col++){
+            t[0][col] = {grid[0][col]*t[0][col-1].first,grid[0][col]*t[0][col-1].second};
+        }
+
+        for(int row = 1;row<m;row++){
+            t[row][0] = {grid[row][0]*t[row-1][0].first,grid[row][0]*t[row-1][0].second};
+        }
+
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                ll upmax = t[i-1][j].first;
+                ll upmin = t[i-1][j].second;
+                ll leftmax = t[i][j-1].first;
+                ll leftmin = t[i][j-1].second;
+
+                t[i][j].first = max({grid[i][j]*(long long)upmax,grid[i][j]*(long long)upmin,grid[i][j]*(long long)leftmax,grid[i][j]*(long long)leftmin});
+                t[i][j].second = min({grid[i][j]*(long long)upmax,grid[i][j]*(long long)upmin,grid[i][j]*(long long)leftmax,grid[i][j]*(long long)leftmin});
+            }
+        }
+
+        return t[m-1][n-1].first < 0 ? -1 : t[m-1][n-1].first % MOD;
+    }
+};

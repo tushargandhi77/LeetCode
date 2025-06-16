df9862c4c0fbfbb6028337ef33f5c4eb926a21a7
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-16 08:43:37 +0530

    Update 2016.cpp

--- 2016.cpp
+++ 2016.cpp
@@ -17,3 +17,41 @@ public:
         return maxDiff;
     }
 };
+
+
+// Method 2 :- Optimul space O(n) and time O(n)
+
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> LRMin(n,0);
+        vector<int> RLMax(n,0);
+
+        for(int i = 0;i<n;i++){
+            if(i == 0){
+                LRMin[i] = nums[i];
+            }
+            else{
+                LRMin[i] = min(nums[i],LRMin[i-1]);
+            }
+        }
+
+        for(int i = n-1;i>=0;i--){
+            if(i == n-1){
+                RLMax[i] = nums[i];
+            }
+            else{
+                RLMax[i] = max(nums[i],RLMax[i+1]);
+            }
+        }
+        int maxDiff = -1;
+        for(int i = 0;i<n;i++){
+            if(LRMin[i] < RLMax[i]){
+                maxDiff = max(maxDiff,RLMax[i]-LRMin[i]);
+            }
+        }
+
+        return maxDiff;
+    }
+};

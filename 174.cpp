58b4af1b474d6a464e9a1af6dcc0edffc6b98604
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-17 11:49:39 +0530

    Update 174.cpp

--- 174.cpp
+++ 174.cpp
@@ -162,3 +162,39 @@ public:
         return solve(0,0,dungeon);
     }
 };
+
+// Method 5: -  Bottom Up
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size();
+        int n = dungeon[0].size();
+
+        vector<vector<int>> t(m,vector<int>(n,0));
+
+        for(int i = m-1;i>=0;i--){
+            for(int j = n-1;j>=0;j--){
+                if(i == m-1 && j == n-1){
+                    if(dungeon[i][j] > 0){
+                        t[i][j] =  1;
+                    }
+                    else{
+                        t[i][j] = abs(dungeon[i][j]) + 1; 
+                    }
+                }
+
+                else{
+                    int right = j + 1 >= n ? 1e9 : t[i][j+1];
+                    int down = i + 1 >= m ? 1e9 : t[i+1][j];
+
+                    int result = min(right,down) - dungeon[i][j];
+
+                    t[i][j] = result <= 0 ? 1 : result;
+                }
+            }
+        }
+
+        return t[0][0];
+    }
+};

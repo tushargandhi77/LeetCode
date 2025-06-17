fa2b67e3db5a1c1b98145d3390160b3b8462cef2
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-17 10:11:01 +0530

    Update 174.cpp

--- 174.cpp
+++ 174.cpp
@@ -40,3 +40,51 @@ public:
         return result;
     }
 };
+
+// Method 2 : Brute force Memo
+
+class Solution {
+public:
+    int m,n;
+
+    unordered_map<string,bool> mp;
+    bool cansurvive(int i,int j,int mid,vector<vector<int>>& dungeon){
+        if(i >= m || j >= n) return false;
+
+        mid += dungeon[i][j];
+
+        if(mid <= 0) return false;
+
+        if(i == m-1 && j == n-1) return true;
+
+        string key = to_string(i) + "_" + to_string(j) + "_" + to_string(mid);
+
+        if(mp.count(key)) return mp[key];
+
+        return mp[key] = cansurvive(i+1,j,mid,dungeon) | cansurvive(i,j+1,mid,dungeon);
+    }
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        m = dungeon.size();
+        n = dungeon[0].size();
+
+
+        int left = 1;
+        int right = 4 * 1e7;
+
+        int result = 4 * 1e7;
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            mp.clear();
+            if(cansurvive(0,0,mid,dungeon)){
+                result = mid;
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+};

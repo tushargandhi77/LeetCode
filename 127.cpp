f207e05f1835b5177b6f15710208d9477fb69373
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-19 11:34:51 +0530

    Create 127.cpp

---
+++ 127.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> wordlist(begin(wordList),end(wordList));
+        unordered_set<string> visited;
+
+        queue<string> que;
+        que.push(beginWord);
+
+        int level = 1;
+
+        while(!que.empty()){
+            int N = que.size();
+            while(N--){
+                string curr = que.front();
+                que.pop();
+
+                if(curr == endWord) return level;
+
+                for(int i = 0;i<curr.length();i++){
+                    for(char ch = 'a';ch<='z';ch++){
+                        char old = curr[i];
+
+                        curr[i] = ch;
+                        if(!visited.count(curr) && wordlist.count(curr)){
+                            que.push(curr);
+                            visited.insert(curr);
+                        }
+                        curr[i] = old;
+                    }
+                }
+            }
+            level++;
+        }
+
+        return 0;
+
+    }
+};

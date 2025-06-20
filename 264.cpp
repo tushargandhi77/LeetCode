faeebb4a8857ee0aec36f8c17d821a86eb901b18
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-20 15:44:02 +0530

    Update 264.cpp

--- 264.cpp
+++ 264.cpp
@@ -31,3 +31,36 @@ public:
 };
 
 // Optimul
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> arr(n+1);
+
+        int i2 = 1;
+        int i3 = 1;
+        int i5 = 1;
+
+        arr[1] = 1;
+
+        for(int i = 2;i<=n;i++){
+            int i2ugly = arr[i2] * 2;
+            int i3ugly = arr[i3] * 3;
+            int i5ugly = arr[i5] * 5;
+
+            int minugly = min({i2ugly,i3ugly,i5ugly});
+            arr[i] = minugly;
+
+            if(minugly == i2ugly){
+                i2++;
+            }
+            if(minugly == i3ugly){
+                i3++;
+            }
+            if(minugly == i5ugly){
+                i5++;
+            }
+        }
+
+        return arr[n];
+    }
+};

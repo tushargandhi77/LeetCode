301d5f671f3384b46294036a0a49f98e849b3fb9
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-24 11:19:16 +0530

    Update 2200.cpp

--- 2200.cpp
+++ 2200.cpp
@@ -17,3 +17,32 @@ public:
         return result;
     }
 };
+
+// Method 2 optimul
+
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> result;
+
+        int n = nums.size();
+
+
+        for(int j = 0;j<n;j++){
+            if(nums[j] == key){
+                int start = max(0,j-k);
+                int end = min(j+k,n-1);
+
+                if(!result.empty() && result.back() >= start){
+                    start = result.back() + 1;
+                }
+
+                for(int i = start;i<=end;i++){
+                    result.push_back(i);
+                }
+            }
+        }
+
+        return result;
+    }
+};

b83302773c825bef969a3fb4ce4b80dcf7f663e7
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-28 13:22:55 +0530

    Update 1334.cpp

--- 1334.cpp
+++ 1334.cpp
@@ -75,3 +75,69 @@ public:
         return final_result;
     }
 };
+
+
+// Floyd warshell 
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> distance(n,vector<int>(n,1e9));
+
+
+        for(auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            distance[u][v] = wt;
+            distance[v][u] = wt;
+        }
+
+
+        // Floyd Warshell
+        for(int i = 0;i<n;i++){
+            distance[i][i] = 0;
+        }
+
+        for(int via = 0;via<n;via++){
+            for(int i = 0;i<n;i++){
+                for(int j = 0;j<n;j++){
+                    if(i == j) continue;
+
+                    distance[i][j] = min(distance[i][j],distance[i][via] + distance[via][j]);
+                }
+            }
+        }
+
+        // logic
+
+        int min_pts = INT_MAX;
+
+        for(int i = 0;i<n;i++){
+            int count_node = 0;
+            for(int j = 0;j<n;j++){
+                if(i == j) continue;
+
+                if(distance[i][j] <= distanceThreshold) count_node ++;
+            }
+
+            min_pts = min(min_pts,count_node);
+        }
+
+        int result = -1;
+        for(int i = 0;i<n;i++){
+            int count_node = 0;
+            for(int j = 0;j<n;j++){
+                if(i == j) continue;
+
+                if(distance[i][j] <= distanceThreshold) count_node ++;
+            }
+            if(count_node == min_pts){
+                result = i;
+            }
+        }
+
+        return result;
+    }
+};

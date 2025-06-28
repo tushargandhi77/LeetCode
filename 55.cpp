2028cb540b11c50fd60952db34f07f5e7aafc333
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-28 12:19:52 +0530

    Update 55.cpp

--- 55.cpp
+++ 55.cpp
@@ -1,4 +1,4 @@
-// Recusion 
+// Recusion + memo
 
 class Solution {
 public:
@@ -20,5 +20,30 @@ public:
     bool canJump(vector<int>& nums) {
         memset(t,-1,sizeof(t));
         return solve(0,nums);
+    }
+};
+
+
+// Bottom Up
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> t(n,false);
+
+        t[0] = true;
+
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(t[j] == true && j + nums[j] >= i){
+                    t[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return t[n-1];
     }
 };

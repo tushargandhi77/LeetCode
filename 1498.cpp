000564f41e32d8ec3db79103ca4e9a8488726e92
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-29 13:55:32 +0530

    Update 1498.cpp

--- 1498.cpp
+++ 1498.cpp
@@ -1,4 +1,40 @@
+// Optimul
 
+class Solution {
+public:
+    int M = 1e9+7;
+
+    
+    int numSubseq(vector<int>& nums, int target) {
+        int result = 0;
+        int n = nums.size();
+
+        sort(begin(nums),end(nums));
+
+        vector<int> power(n);
+
+        power[0] = 1;
+
+        for(int i = 1;i<n;i++){
+            power[i] = (power[i-1] * 2) % M;
+        }
+
+        int i = 0;
+        int j = n - 1;
+
+        while(i <= j){
+            if(nums[i]+nums[j] <= target){
+                result = (result % M + power[j-i])%M;
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+
+        return result;
+    }
+};
 
 
 // Backtracking

1f33865441c7fb79afd2ce993420ab28e9e8631f
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-30 15:42:45 +0530

    Update 2045.cpp

--- 2045.cpp
+++ 2045.cpp
@@ -47,3 +47,55 @@ public:
         return -1;
     }
 };
+
+// BFS
+
+class Solution {
+public:
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        unordered_map<int,vector<int>> adj;
+
+        for(auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> d1(n+1,INT_MAX);
+        vector<int> d2(n+1,INT_MAX);
+
+        queue<pair<int,int>> que;
+
+        que.push({1,1}); // node freq
+
+        d1[1] = 0;
+
+        while(!que.empty()){
+            auto [node,freq] = que.front();
+            que.pop();
+
+            int timepassed = (freq==1) ? d1[node] : d2[node];
+
+            if(node == n && d2[node] != INT_MAX) return d2[node];
+
+            int div = timepassed / change;
+            if(div%2 == 1){
+                timepassed = (div+1)*change;
+            }
+
+            for(auto& nbr: adj[node]){
+                if(d1[nbr] == INT_MAX){
+                    d1[nbr] = timepassed+time;
+                    que.push({nbr,1});
+                }
+                else if(d2[nbr] == INT_MAX && d1[nbr] != timepassed+time){
+                    d2[nbr] = timepassed+time;
+                    que.push({nbr,2});
+                }
+            }
+        }
+        return -1;
+    }   
+};

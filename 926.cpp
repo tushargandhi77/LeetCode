37d76095aa44b18e1aefd9f819837310e1c91dbf
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-06-30 13:50:07 +0530

    Create 926.cpp

---
+++ 926.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int t[100001][2];
+    int solve(int currIdx,int prev,string& s){
+        if(currIdx >= s.length()) return 0;
+
+        if(t[currIdx][prev] != -1) return t[currIdx][prev];
+
+        int flip = INT_MAX;
+        int non_flip = INT_MAX;
+        if(s[currIdx] == '0'){
+            if(prev == 0){
+                flip = 1 + solve(currIdx+1,1,s);
+                non_flip = solve(currIdx+1,0,s);
+            }
+            else{
+                flip = 1 + solve(currIdx+1,1,s);
+            }
+        }
+        else{
+            if(prev == 0){
+                flip = 1 + solve(currIdx+1,0,s);
+                non_flip = solve(currIdx+1,1,s);
+            }
+            else{
+                non_flip = solve(currIdx+1,1,s);
+            }
+        }
+
+        return t[currIdx][prev] = min(flip,non_flip);
+    }
+    int minFlipsMonoIncr(string s) {
+        memset(t,-1,sizeof(t));
+        return solve(0,0,s);
+    }
+};

d81f9c91174ea2c436ccf923e5c046aba8f04078
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-01 14:21:03 +0530

    Update 1402.cpp

--- 1402.cpp
+++ 1402.cpp
@@ -19,3 +19,36 @@ public:
         return solve(0,1,satisfaction);
     }
 };
+
+
+// BU
+
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int n = satisfaction.size();
+
+        sort(begin(satisfaction),end(satisfaction));
+
+        vector<vector<long long>> t(n,vector<long long>(n+1,INT_MIN));
+
+        for(int i = 0;i<n;i++){
+            t[i][0] = 0;
+        }
+
+        t[0][1] = satisfaction[0];
+
+        for(int i = 1;i<n;i++){
+            for(int time=1;time<=n;time++){
+                long long include = satisfaction[i]*time + t[i-1][time-1];
+
+                long long exclude = t[i-1][time];
+
+                t[i][time] = max(include,exclude);
+            }
+        }
+
+        return *max_element(begin(t[n-1]),end(t[n-1]));
+
+    }
+};

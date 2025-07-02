0c18919f24d7d4e7bf933b4cce965ff1f4667751
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-02 22:41:52 +0530

    Update 3333.cpp

--- 3333.cpp
+++ 3333.cpp
@@ -117,3 +117,64 @@ public:
         return (P - invalidCount + M ) % M;
     }
 };
+
+// Bottom Up
+
+class Solution {
+public:
+    int M = 1e9 + 7;
+    
+    int possibleStringCount(string word, int k) {
+        if(k > word.length()) return 0;
+
+        vector<int> freq;
+        int count = 1;
+
+        for(int i = 1;i<word.length();i++){
+            if(word[i] == word[i-1]){
+                count++;
+            }
+            else{
+                freq.push_back(count);
+                count = 1;
+            }
+        }
+        freq.push_back(count);
+
+        long long P = 1;
+
+        for(int& f: freq){
+            P = (P * f) % M;
+        }
+
+        if(freq.size() >= k){
+            return P;
+        }
+        int n = freq.size();
+
+        vector<vector<int>> t(n+1,vector<int>(k+1,0));
+
+        for(int count = k-1;count>=0;count--){
+            t[n][count] = 1;
+        }
+
+        for(int i = n-1;i>=0;i--){
+            for(int count = k-1;count>=0;count--){
+                
+                long long result = 0;
+                for(int take = 1;take<=freq[i];take++){
+                    if(count+take < k){
+                        result  = (result + t[i+1][count+take])%M;
+                    }
+                }
+
+                t[i][count] = result;
+
+            }
+        }
+
+        long long invalidCount = t[0][0];
+
+        return (P - invalidCount + M ) % M;
+    }
+};

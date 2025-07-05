960afaa0f4cfab870b07a4ec778307b938b8832c
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-05 13:56:20 +0530

    Update 2218.cpp

--- 2218.cpp
+++ 2218.cpp
@@ -25,3 +25,31 @@ public:
         return solve(0,piles,k);
     }
 };
+
+
+// Bottom Up
+
+class Solution {
+public:
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        int n = piles.size();
+
+        vector<vector<int>> t(n+1,vector<int>(k+1,0));
+
+        for(int i = 1;i<=n;i++){
+            for(int coins = 0;coins<=k;coins++){
+                int sum = 0;
+
+                for(int curr = 0;curr<=min((int)piles[i-1].size(),coins);curr++){
+                    if(curr > 0){
+                        sum += piles[i-1][curr-1];
+                    }
+
+                    t[i][coins] = max(t[i][coins],sum + t[i-1][coins - curr]);
+                }
+            }
+        }
+
+        return t[n][k];
+    }
+};

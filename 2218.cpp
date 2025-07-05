8b4e48d85cd9881b8b1b52b280b79b93bc9472c9
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-05 13:37:22 +0530

    Create 2218.cpp

---
+++ 2218.cpp
@@ -0,0 +1,27 @@
+// Dynamic Programming :- Recursion + Memo
+
+class Solution {
+public:
+    int t[1001][2001];
+    int solve(int idx,vector<vector<int>>& piles,int k){
+        if(k < 0) return 0;
+        if(idx  >= piles.size()) return 0;
+
+        if(t[idx][k] != -1) return t[idx][k];
+
+        int not_taken = solve(idx+1,piles,k);
+
+        int taken = INT_MIN,sum = 0;
+        for(int j = 0; j<min((int)piles[idx].size(),k);j++){
+            sum += piles[idx][j];
+
+            taken = max(taken,sum + solve(idx+1,piles,k-(j+1)));
+        }
+
+        return t[idx][k] = max(taken,not_taken);
+    }
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        memset(t,-1,sizeof(t));
+        return solve(0,piles,k);
+    }
+};

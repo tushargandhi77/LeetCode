3cf5b03e47001a62abdd3c4b5447922e5ea510d9
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-07 11:05:02 +0530

    Update 1353.cpp

--- 1353.cpp
+++ 1353.cpp
@@ -11,6 +11,11 @@ public:
         int i = 0;
 
         while(!pq.empty() || i < n){
+            
+            if(pq.empty()){
+                day = events[i][0];
+            }
+
             while(i < n && events[i][0] == day){
                 pq.push(events[i][1]);
                 i++;

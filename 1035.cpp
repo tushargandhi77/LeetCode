a525a6ebc1e030108b202831f120cdb97560f679
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-11 16:46:19 +0530

    Update 1035.cpp

--- 1035.cpp
+++ 1035.cpp
@@ -2,7 +2,6 @@
 
 class Solution {
 public:
-    int n;
     int t[501][501];
     int solve(int i,int j,vector<int>&nums1,vector<int>& nums2){
         if(i >= nums1.size() || j >= nums2.size()) return 0;
@@ -19,8 +18,35 @@ public:
         return t[i][j] = max(option1,option2);
     }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        n = nums1.size();
         memset(t,-1,sizeof(t));
         return solve(0,0,nums1,nums2);
+    }
+};
+
+
+// Bu
+
+class Solution {
+public:
+    
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+       int n1 = nums1.size();
+       int n2 = nums2.size();
+
+
+       vector<vector<int>> t(n1+1,vector<int>(n2+1,0));
+
+       for(int i = 1;i<=n1;i++){
+        for(int j = 1;j<=n2;j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+       }
+
+       return t[n1][n2];
     }
 };

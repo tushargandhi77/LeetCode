b617a16090754f0fc0ffbd416a345e6237dcccd9
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-14 13:47:08 +0530

    Update 1290.cpp

--- 1290.cpp
+++ 1290.cpp
@@ -32,3 +32,45 @@ public:
         return result;
     }
 };
+
+
+// M2 Revuse using recursion
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* reverselist(ListNode* head){
+        if(head == nullptr || head->next == nullptr) return head;
+
+        ListNode* last = reverselist(head->next);
+
+        head->next->next = head;
+        head->next = nullptr;
+
+        return last;
+    }
+    int getDecimalValue(ListNode* head) {
+        ListNode* prev = reverselist(head);
+
+        int result = 0;
+        int power = 0;
+
+        while(prev){
+            int val = prev->val;
+            result  += (val * (int)pow(2,power));
+            power++;
+            prev = prev->next;
+        }
+
+        return result;
+    }
+};

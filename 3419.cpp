63186b3d5064311795026e98f7cce8eeb1184bb5
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-15 16:07:08 +0530

    Update 3419.cpp

--- 3419.cpp
+++ 3419.cpp
@@ -1,5 +1,4 @@
 // DFS
-
 class Solution {
 public:
     void DFS(int u,unordered_map<int,vector<pair<int,int>>>& adj,vector<bool>& visited,int maxwt){
@@ -57,3 +56,64 @@ public:
 };
 
 // BFS
+class Solution {
+public:
+    bool canwemake(unordered_map<int,vector<pair<int,int>>>& adj,int n,int maxwt){
+        vector<bool> visited(n,false);
+        queue<int> que;
+
+        que.push(0);
+        visited[0] = true;
+
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+
+            for(auto& it: adj[u]){
+                int v = it.first;
+                int wt = it.second;
+
+                if(wt <= maxwt && !visited[v]){
+                    que.push(v);
+                    visited[v] = true;
+                }
+            }
+        }
+
+        for(int i = 0;i<n;i++){
+            if(visited[i] == false) return false;
+        }
+
+        return true;
+    }
+    int minMaxWeight(int n, vector<vector<int>>& edges, int threshold) {
+        int l = 0;
+        int r = INT_MIN;
+
+        unordered_map<int,vector<pair<int,int>>> adj;
+
+        for(auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adj[v].push_back({u,wt});
+            r = max(wt,r);
+        }   
+
+        int result = -1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+
+            if(canwemake(adj,n,mid)){
+                result = mid;
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+
+        return result;
+    }
+};

209352a2a5415f5f995b4e964cc4c3eaa329f15f
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-20 16:57:16 +0530

    Update 956.cpp

--- 956.cpp
+++ 956.cpp
@@ -25,3 +25,32 @@ public:
         return solve(0,0,0,rods);
     }
 };
+
+
+// 
+
+class Solution {
+public:
+    int t[21][10001];
+    int solve(int idx,int diff,vector<int>& rods){
+        if(idx >= rods.size()){
+            if(diff == 0) return 0;
+
+            return INT_MIN;
+        }
+
+        if(t[idx][diff+5000] != -1) return t[idx][diff+5000]; // diff may negative
+
+        int take_l1 = rods[idx] + solve(idx+1,diff+rods[idx],rods); // l1 - l2 = diff, l1 + x - l2 => l1-l2 + x => diff + x
+
+        int take_l2 = rods[idx] + solve(idx+1,diff-rods[idx],rods); // l1 - l2 = diff, l1  - (l2 + x)  => l1-l2 - x => diff - x
+
+        int nothing = solve(idx+1,diff,rods);
+
+        return t[idx][diff+5000] = max({take_l1,take_l2,nothing});
+    }
+    int tallestBillboard(vector<int>& rods) {
+        memset(t,-1,sizeof(t));
+        return solve(0,0,rods)/2; // because solve return full length and answer will be half
+    }
+};

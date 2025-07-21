495dbe0291b0d7e7f32fcd4a30030188b32c1b4f
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-21 11:14:16 +0530

    Update 1957.cpp

--- 1957.cpp
+++ 1957.cpp
@@ -20,3 +20,29 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string result = "";
+        int freq = 1;
+
+        result.push_back(s[0]);
+
+        for(int i = 1;i<s.length();i++){
+            if(s[i] == result.back()){
+                freq++;
+                if(freq <= 2){
+                    result.push_back(s[i]);
+                }
+            }
+            else{
+                freq = 1;
+                result.push_back(s[i]);
+            }
+        }
+
+        return result;
+    }
+};

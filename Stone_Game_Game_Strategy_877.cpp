d85744d39b364621ab61f913053bcc2e7f125267
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-31 14:34:44 +0530

    Update Stone_Game_Game_Strategy_877.cpp

--- Stone_Game_Game_Strategy_877.cpp
+++ Stone_Game_Game_Strategy_877.cpp
@@ -1,3 +1,4 @@
+// Method 1
 class Solution {
 public:
     int t[501][501];
@@ -24,5 +25,28 @@ public:
         int bob = total_stones - alice;
 
         return alice > bob;
+    }
+};
+
+// Method 2 
+
+class Solution {
+public:
+    int t[501][501];
+    int solve(int i,int j,vector<int>& piles){
+        if(i > j) return 0;
+        
+        if(i == j) return piles[i];
+
+        if(t[i][j] != -1) return t[i][j];
+
+        int take_i = piles[i] - solve(i+1,j,piles); // calculate the difference of score alice - bob
+        int take_j = piles[j] - solve(i,j-1,piles);
+
+        return t[i][j] = max(take_i,take_j);
+    }
+    bool stoneGame(vector<int>& piles) {
+        memset(t,-1,sizeof(t));
+        return solve(0,piles.size()-1,piles) > 0;
     }
 };

f65a00592c039f9724c8bdb0cd5163a0e4662190
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-07-31 14:23:06 +0530

    Update 486.cpp

--- 486.cpp
+++ 486.cpp
@@ -24,3 +24,24 @@ public:
         return player1 >= player2;
     }
 };
+
+
+class Solution {
+public:
+    int t[21][21];
+    int solve(int i,int j,vector<int>& nums){
+        if(i > j) return 0;
+        if(i == j) return nums[i];
+
+        if(t[i][j] != -1) return t[i][j];
+
+        int take_i = nums[i] - solve(i+1,j,nums);
+        int take_j = nums[j] - solve(i,j-1,nums);
+
+        return t[i][j] = max(take_i,take_j);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
+        return solve(0,nums.size()-1,nums) >= 0;
+    }
+};

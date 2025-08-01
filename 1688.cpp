15261d5c310e6a87f304fede20950b07f8db4182
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-01 16:04:49 +0530

    Create 1688.cpp

---
+++ 1688.cpp
@@ -0,0 +1,21 @@
+// log n
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int result = 0;
+
+        while(n > 1){
+            if(n%2 == 0){
+                result += n/2;
+                n = n/2;
+            }
+            else{
+                result += (n-1)/2;
+                n = (n + 1)/2;
+            }
+        }
+
+        return result;
+    }
+};

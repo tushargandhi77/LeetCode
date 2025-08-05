398b34371b68061bd0a46b5fb1e7ea485ef12dd8
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-05 18:10:27 +0530

    Update 1464.cpp

--- 1464.cpp
+++ 1464.cpp
@@ -7,3 +7,26 @@ public:
         return (nums[n-1] - 1)*(nums[n-2] - 1);
     }
 };
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        
+        int largest = INT_MIN;
+        int second_largest = INT_MIN;
+
+        for(int& num: nums){
+            if(num > largest){
+                second_largest = largest;
+                largest = num;
+            }
+            else if(num > second_largest){
+                second_largest = num;
+            }
+        }
+
+        return (largest - 1)*(second_largest - 1);
+    }
+};

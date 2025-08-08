e1dfca49be401f92453e281fa4bd3aeab11458f2
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-08 17:19:52 +0530

    Update 3363.cpp

--- 3363.cpp
+++ 3363.cpp
@@ -58,3 +58,51 @@ public:
         return c1+c2+c3;
     }
 };
+
+// bottom up
+
+class Solution {
+public:
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int n = fruits.size();
+        vector<vector<int>> t(n,vector<int>(n,0));
+
+        int result = 0;
+        for(int i = 0;i<n;i++){
+            result += fruits[i][i];
+        }
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(i < j && i + j < n-1){
+                    t[i][j] = 0;
+                }
+                else if(i > j && i + j < n-1){
+                    t[i][j] = 0;
+                }
+                else{
+                    t[i][j] = fruits[i][j];
+                }
+            }
+        }
+
+        // child 2
+
+        for(int i = 1;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                t[i][j] += max({t[i-1][j-1],t[i-1][j],j+1 < n ?  t[i-1][j+1] : 0});
+            }
+        }
+
+        for(int j = 1;j<n;j++){
+            for(int i = j+1;i<n;i++){
+                t[i][j] += max({t[i-1][j-1],t[i][j-1],i+1 < n ? t[i+1][j-1] : 0});
+            }
+        }
+
+
+        result += (t[n-1][n-2] + t[n-2][n-1]);
+
+        return result;
+    }
+};

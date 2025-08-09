395fc13a18015a5db43d2fce4db0744df122e190
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-09 12:41:53 +0530

    Update 231.cpp

--- 231.cpp
+++ 231.cpp
@@ -20,3 +20,24 @@ public:
         return 1073741824%n == 0;
     }
 };
+
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+
+        return (n&(n-1)) == 0;
+    }
+};
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+
+        if(n == 1) return true;
+
+        return n % 2 == 0 && isPowerOfTwo(n/2);
+    }
+};

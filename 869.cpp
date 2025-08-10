f51bfa86a16950363207f1cf3cca0ca0ea21fb49
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-10 15:47:01 +0530

    Update 869.cpp

--- 869.cpp
+++ 869.cpp
@@ -65,3 +65,29 @@ public:
         return false;
     }
 };
+
+
+// M4 
+
+class Solution {
+public:
+    int getVectorCount(int n){
+       int num = 0;
+
+        while(n){
+            num += pow(10,n%10);
+            n /= 10;
+        }
+
+        return num;
+    }
+    bool reorderedPowerOf2(int n) {
+        int input = getVectorCount(n);
+
+        for(int p = 0;p<=29;p++){
+            if(input == getVectorCount(1 << p)) return true;
+        }
+
+        return false;
+    }
+};

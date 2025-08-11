906ff2c4d75a3cb650cbab4862da08e24cb9bfaa
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-11 14:55:22 +0530

    Update 1582.cpp

--- 1582.cpp
+++ 1582.cpp
@@ -35,3 +35,49 @@ public:
         return result;
     }
 };
+
+// Better
+
+class Solution {
+public:
+    int m,n;
+
+    void Build(vector<int>& rowSum,vector<int>& colSum,vector<vector<int>>& mat){
+        for(int i = 0;i<m;i++){
+            int sum = 0;
+            for(int j = 0;j<n;j++){
+                sum += mat[i][j];
+            }
+            rowSum[i] = sum;
+        }
+
+        for(int j = 0;j<n;j++){
+            int sum = 0;
+            for(int i = 0;i<m;i++){
+                sum += mat[i][j];
+            }
+            colSum[j] = sum;
+        }
+
+    }
+    int numSpecial(vector<vector<int>>& mat) {
+        m = mat.size();
+        n = mat[0].size();
+
+        vector<int> rowSum(m,0);
+        vector<int> colSum(n,0);
+
+        Build(rowSum,colSum,mat);
+
+        int result = 0;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(mat[i][j] == 1 && rowSum[i] < 2 && colSum[j] < 2){
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+};

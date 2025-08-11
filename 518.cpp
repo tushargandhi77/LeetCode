1bf0a0f580d8228e5c3a3480f99974a8ab482061
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-11 11:41:33 +0530

    Create 518.cpp

---
+++ 518.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int t[301][5002];
+    int solve(int idx,int curramount,int amount,vector<int>& coins){
+        if(curramount > amount) return 0;
+
+        if(curramount == amount) return 1;
+
+        if(t[idx][curramount] != -1) return t[idx][curramount];
+
+
+        int result = 0;
+
+        for(int i = idx;i<coins.size();i++){
+            result += solve(i,min(curramount+coins[i],amount+1),amount,coins);
+        }
+
+        return t[idx][curramount] = result;
+    }
+    int change(int amount, vector<int>& coins) {
+        memset(t,-1,sizeof(t));
+        return solve(0,0,amount,coins);
+    }
+};

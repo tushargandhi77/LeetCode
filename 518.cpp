4a22946c4d262e3011e038c8328ad638c6c803b6
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-11 11:56:05 +0530

    Update 518.cpp

--- 518.cpp
+++ 518.cpp
@@ -22,3 +22,28 @@ public:
         return solve(0,0,amount,coins);
     }
 };
+
+
+class Solution {
+public:
+    int t[301][5002];
+    int solve(int idx,int curramount,int amount,vector<int>& coins){
+        if(curramount == amount) return 1;
+
+        if(idx == coins.size()) return 0;
+
+        if(curramount > amount) return 0;
+
+        if(t[idx][curramount] != -1) return t[idx][curramount];
+
+        int take = solve(idx,min(curramount+coins[idx],amount+1),amount,coins);
+
+        int skip = solve(idx+1,curramount,amount,coins);
+
+        return t[idx][curramount] = take+skip;
+    }   
+    int change(int amount, vector<int>& coins) {
+        memset(t,-1,sizeof(t));
+        return solve(0,0,amount,coins);
+    }
+};

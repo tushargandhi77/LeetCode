f29b97f2e8740791f9dfc6470c8e1e999f558405
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-12 17:16:46 +0530

    Update 1913.cpp

--- 1913.cpp
+++ 1913.cpp
@@ -10,3 +10,36 @@ public:
         return maxD - minD;
     }
 };
+
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        int n = nums.size();
+        int firstMax = INT_MIN;
+        int secondMax = INT_MIN;
+
+        int firstMin = INT_MAX;
+        int secondMin = INT_MAX;
+
+        for(int& num: nums){
+            if(num > secondMax){
+                firstMax = secondMax;
+                secondMax = num;
+            }
+            else if(num > firstMax){
+                firstMax = num;
+            }
+
+            if(num < secondMin){
+                firstMin = secondMin;
+                secondMin = num;
+            }
+            else if(num < firstMin){
+                firstMin = num;
+            }
+        }
+       
+
+       return firstMax*secondMax - firstMin * secondMin;
+    }
+};

28759fa23f49c3e37db77b3f2c55b1af1b6ec90b
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-13 14:32:51 +0530

    Update 2706.cpp

--- 2706.cpp
+++ 2706.cpp
@@ -8,3 +8,26 @@ public:
         return money - Minprice < 0 ? money : money - Minprice;
     }
 };
+
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int firstMin = INT_MAX;
+        int secondMin = INT_MAX;
+
+        for(int& num: prices){
+            if(num < firstMin){
+                secondMin = firstMin;
+                firstMin = num;
+            }
+            else if(num < secondMin){
+                secondMin = num;
+            }
+        }
+
+        int Minprice = firstMin + secondMin;
+
+        return money - Minprice < 0 ? money : money - Minprice;
+    }
+};

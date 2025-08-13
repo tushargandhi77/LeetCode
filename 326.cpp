5dc778bcee6a37b1089c7f11eb7943c93313fc85
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-13 09:08:36 +0530

    Update 326.cpp

--- 326.cpp
+++ 326.cpp
@@ -8,3 +8,15 @@ public:
         return n % 3 == 0 && isPowerOfThree(n/3);
     }
 };
+
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0) return false;
+
+        if(n == 1) return true;
+
+        return n % 3 == 0 && isPowerOfThree(n/3);
+    }
+};

2438d0200ecd19bafe94a9639972257057dde7ed
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-14 09:46:47 +0530

    Update 2264.cpp

--- 2264.cpp
+++ 2264.cpp
@@ -16,3 +16,22 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        char maxChar =  ' ';
+        int n = num.length();
+        for(int i = 1;i<n-1;i++){
+            if(num[i] == num[i-1] && num[i] == num[i+1]){
+                maxChar = max(maxChar,num[i]);
+            }
+        }
+
+
+        if(maxChar == ' ') return "";
+
+        return string(3,maxChar);
+    }
+};

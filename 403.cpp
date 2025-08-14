9a5e69fd37efc8883ed6d6c4821601be260a70ab
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-14 13:45:26 +0530

    Update 403.cpp

--- 403.cpp
+++ 403.cpp
@@ -35,3 +35,45 @@ public:
         return solve(mp[0],0,stones);
     }
 };
+
+
+class Solution {
+public:
+    int maxStone;
+    unordered_set<int> st;
+    unordered_map<string,bool> mp;
+    bool solve(int curr_stone,int jump){
+        if(curr_stone == maxStone) return true;
+
+        string key = to_string(curr_stone) + "_" + to_string(jump);
+
+        if(mp.count(key)) return mp[key];
+
+        bool result = false;
+
+        for(int next_jump = jump-1;next_jump <= jump+1;next_jump++){
+            if(next_jump > 0){
+                int next_stone = curr_stone + next_jump;
+
+                if(st.count(next_stone)){
+                    result = result | solve(next_stone,next_jump);
+                }
+            }
+        }
+
+        return mp[key] = result;
+    }
+    bool canCross(vector<int>& stones) {
+        int n = stones.size();
+
+        if(stones[1] != 1) return false;
+
+        maxStone = stones[n-1];
+
+        for(int& num: stones){
+            st.insert(num);
+        }
+
+        return solve(0,0);
+    }
+};

478da6e8e6479e31a4d7fb72fd8f79beef9e332b
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-19 10:42:24 +0530

    Update 2348.cpp

--- 2348.cpp
+++ 2348.cpp
@@ -22,3 +22,26 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long result = 0;
+        int n = nums.size();
+        
+        long long count  = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] == 0){
+                count++;
+            }
+            else{
+                count = 0;
+            }
+
+            result += count;
+        }
+
+        return result;
+    }
+};

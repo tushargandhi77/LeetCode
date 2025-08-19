f975faec6c2a715183fdd3727a6b21781e4d1f4e
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-19 10:35:37 +0530

    Create 2348.cpp

---
+++ 2348.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long result = 0;
+        int n = nums.size();
+        
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] == 0){
+                count++;
+            }
+            else{
+                result += ((long long)count*(count+1))/2;
+                count = 0;
+            }
+        }
+
+        if(count > 0){
+            result += ((long long)count*(count+1))/2;
+        }
+
+        return result;
+    }
+};

6cf1393dc3ce8a04a0b1e404f4d9c12662e6fd2e
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-08-30 21:57:40 +0530

    Update 1758.cpp

--- 1758.cpp
+++ 1758.cpp
@@ -26,3 +26,25 @@ public:
         return min(start_with_0,start_with_1);
     }
 };
+
+class Solution {
+public:
+    int minOperations(string s) {
+        int start_with_0 = 0; // 010101...
+
+        for(int i = 0;i<s.length();i++){
+            if(i%2 == 0){//Even
+                if(s[i] == '1'){
+                    start_with_0++;
+                }
+            }
+            else{//ODD
+                if(s[i] == '0'){
+                    start_with_0++;
+                }
+            }
+        }   
+        int n = s.length();
+        return min(start_with_0,n-start_with_0);
+    }
+};

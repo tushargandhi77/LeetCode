b007e458aaa328aab525ca229807d03ae801058e
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-01 14:54:28 +0530

    Update 1897.cpp

--- 1897.cpp
+++ 1897.cpp
@@ -23,3 +23,28 @@ public:
         return true;
     }
 };
+// M2
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        vector<int> mp(26,0);
+
+        int n = words.size();
+
+        if(n == 1) return true;
+
+        for(string s: words){
+            for(char ch: s){
+                mp[ch-'a']++;
+            }
+        }
+
+        for(int len: mp){
+
+            if(len%n != 0) return false;
+        }
+
+        return true;
+    }
+};

7e837a027b25c60d467fca9c05a9e98312e61a5c
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-01 14:44:32 +0530

    Update 799.cpp

--- 799.cpp
+++ 799.cpp
@@ -29,3 +29,25 @@ public:
         return min(1.0,solve(poured,query_row,query_glass));
     }
 };
+
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> t(101,vector<double>(101));
+
+        t[0][0] = (double)poured;
+
+        for(int row = 0;row<=query_row;row++){
+            for(int col = 0;col<=row;col++){
+                double extra = (t[row][col] - 1)/2.0;
+
+                if(extra > 0){
+                    t[row+1][col+1] += extra;
+                    t[row+1][col] += extra;
+                }
+            }
+        }
+
+        return min(1.0,t[query_row][query_glass]);
+    }
+};

d534d1c43a6d1ef6325f31ca7ea6367ca7b9c351
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-01 14:57:00 +0530

    Update 1897.cpp

--- 1897.cpp
+++ 1897.cpp
@@ -48,3 +48,28 @@ public:
         return true;
     }
 };
+
+// M3 
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        vector<int> mp(26,0);
+
+        int n = words.size();
+
+        if(n == 1) return true;
+
+        for(string s: words){
+            for(char ch: s){
+                mp[ch-'a']++;
+            }
+        }
+
+        auto lambda = [&](int freq){
+            return freq%n == 0;
+        };
+
+        return all_of(begin(mp),end(mp),lambda);
+    }
+};

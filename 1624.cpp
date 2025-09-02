c012436ddac4994f39ca466a20ccc62acac0bab5
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-02 12:56:05 +0530

    Update 1624.cpp

--- 1624.cpp
+++ 1624.cpp
@@ -38,3 +38,25 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        unordered_map<char,int> mp;
+        int n = s.length();
+        int result = -1;
+
+        for(int i = 0;i<n;i++){
+            char ch = s[i];
+
+            if(mp.find(ch) == mp.end()){
+                mp[ch] = i;
+            }
+            else{
+                result = max(result,i-mp[ch]-1);
+            }
+        } 
+
+        return result;
+    }
+};

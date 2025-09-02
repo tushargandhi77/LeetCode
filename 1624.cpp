a2d8c30e8275bdf4f8227e867e12bf8e985da4bf
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-02 12:48:05 +0530

    Update 1624.cpp

--- 1624.cpp
+++ 1624.cpp
@@ -19,3 +19,22 @@ public:
 };
 
 
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int n = s.length();
+
+        int result = -1;
+        for(int i = 0;i<n;i++){
+            char ch = s[i];
+            
+            int idx = s.find_last_of(ch);
+
+            if(idx == i) continue;
+
+            result = max(result,idx-i-1);
+        }
+
+        return result;
+    }
+};

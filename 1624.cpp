89591c60304ca304acf45c3d9613e4ea058cec8f
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-02 12:58:18 +0530

    Update 1624.cpp

--- 1624.cpp
+++ 1624.cpp
@@ -60,3 +60,25 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        vector<int> freq(26,-1);
+        int n = s.length();
+        int result = -1;
+
+        for(int i = 0;i<n;i++){
+            char ch = s[i];
+            int idx = ch-'a';
+            if(freq[idx] == -1){
+                freq[idx] = i;
+            }
+            else{
+                result = max(result,i-freq[idx]-1);
+            }
+        } 
+
+        return result;
+    }
+};

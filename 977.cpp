d66e245236c81a49ec4ff6dfe5bd3c1bbe8757ab
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-04 16:00:17 +0530

    Update 977.cpp

--- 977.cpp
+++ 977.cpp
@@ -9,3 +9,32 @@ public:
         return nums;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        int i = 0 , j=n-1;
+        int k = n-1;
+
+        while(k >= 0){
+            int a = nums[i]*nums[i];
+            int b = nums[j]*nums[j];
+
+            if(a > b){
+                result[k] = a;
+                i++;
+            }
+            else{
+                result[k]=b;
+                j--;
+            }
+            k--;
+        }
+
+        return result;
+    }
+};

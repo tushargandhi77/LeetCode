f14a9fc23beb79c8ad22416c706d2629f069fe78
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-05 13:02:03 +0530

    Create 1269.cpp

---
+++ 1269.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int MOD = 1e9+7;
+    unordered_map<string,int> mp;
+    int solve(int idx,int steps,int arrLen){
+        if(steps == 0){
+            if(idx == 0) return 1;
+            return 0;
+        }
+        
+        string key = to_string(idx) + "_" + to_string(steps);
+
+        if(mp.count(key)) return mp[key];
+
+        long stay = solve(idx,steps-1,arrLen);
+        
+        long left = idx-1 >= 0 ? solve(idx-1,steps-1,arrLen)%MOD : 0;
+
+        long right = idx+1 < arrLen ? solve(idx+1,steps-1,arrLen)%MOD : 0;
+
+        return mp[key] = (stay + left + right)%MOD;
+    }
+    int numWays(int steps, int arrLen) {
+        return solve(0,steps,arrLen);
+    }
+};

e825e037110c01fee4068253220d5fc5535b9133
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-05 16:32:41 +0530

    Update 2749.cpp

--- 2749.cpp
+++ 2749.cpp
@@ -18,3 +18,23 @@ public:
         return -1;
     }
 };
+// M2
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+            
+            for(int t = 1;t<=36;t++){
+            long long val = (long long)num1 - t*(long long)num2;
+
+            if(val < 0) return -1;
+
+            if(__builtin_popcountll(val) <= t && t <= val){
+                return t;
+            }
+        }
+
+        return -1;
+    }
+};
+
+(t+1)*10^9 >= 2^t => t <= 35.07

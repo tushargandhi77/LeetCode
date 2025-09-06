12e1d6eabd6801cbb5af5ab0ca0b2a923a9c29a4
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-06 21:31:42 +0530

    Update 2540.cpp

--- 2540.cpp
+++ 2540.cpp
@@ -48,3 +48,27 @@ public:
         return -1;
     }
 };
+
+// M3 
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        int i = 0;
+        int j = 0;
+        while(i < m && j < n){
+            if(nums1[i] == nums2[j]) return nums1[i];
+
+            if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+
+        return -1;
+    }
+};

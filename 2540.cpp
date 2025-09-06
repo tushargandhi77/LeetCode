00f7492bbb06c8bf595725d975c60e2def4a97f3
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-06 21:21:10 +0530

    Update 2540.cpp

--- 2540.cpp
+++ 2540.cpp
@@ -14,3 +14,37 @@ public:
         return -1;
     }
 };
+
+// M2 
+class Solution {
+public:
+    bool binarysearch(int num,vector<int>& nums){
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+
+            if(num == nums[mid]) return true;
+            if(num < nums[mid]){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+
+        return false;
+    }
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+
+        for(int i = 0;i<n;i++){
+            int num = nums1[i];
+
+            if(binarysearch(num,nums2)) return num;
+        }
+
+        return -1;
+    }
+};

9cc9aa344ab8a7ea3dbb90381f4bbf5b99e8e0a6
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-08 15:09:54 +0530

    Update 2920.cpp

--- 2920.cpp
+++ 2920.cpp
@@ -28,3 +28,42 @@ public:
         return DFS(0,-1,0,adj,coins,k);
     }
 };
+
+
+// 3D DP
+
+class Solution {
+public:
+    unordered_map<int,unordered_map<int,unordered_map<int,int>>> mp;
+    int DFS(int i,int parent,int power,unordered_map<int,vector<int>>& adj,vector<int>& coins, int k){
+        if(power >= 14) return 0;
+
+        if(mp.count(i) && mp[i].count(parent) && mp[i][parent].count(power)){
+            return mp[i][parent][power];
+        }
+
+        int case1 = (coins[i] >> power) - k;
+        int case2 = coins[i] >> (power+1);
+
+        for(int& ngbr: adj[i]){
+            if(ngbr != parent){
+                case1 += DFS(ngbr,i,power,adj,coins,k);
+                case2 += DFS(ngbr,i,power+1,adj,coins,k);
+            }
+        }
+
+        return mp[i][parent][power] = max(case1,case2);
+    }
+    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int k) {
+        unordered_map<int,vector<int>> adj;
+
+        for(auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        return DFS(0,-1,0,adj,coins,k);
+    }
+};

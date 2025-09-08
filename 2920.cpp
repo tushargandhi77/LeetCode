665bbe6e9a0b7d02f40a44346be01ca237e86ff8
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-08 15:16:56 +0530

    Update 2920.cpp

--- 2920.cpp
+++ 2920.cpp
@@ -67,3 +67,41 @@ public:
         return DFS(0,-1,0,adj,coins,k);
     }
 };
+
+
+// Using 2D DP
+
+class Solution {
+public:
+    int t[100001][14];
+    int DFS(int i,int parent,int power,unordered_map<int,vector<int>>& adj,vector<int>& coins, int k){
+        if(power >= 14) return 0;
+
+        if(t[i][power] != -1) return t[i][power];
+
+        int case1 = (coins[i] >> power) - k;
+        int case2 = coins[i] >> (power+1);
+
+        for(int& ngbr: adj[i]){
+            if(ngbr != parent){
+                case1 += DFS(ngbr,i,power,adj,coins,k);
+                case2 += DFS(ngbr,i,power+1,adj,coins,k);
+            }
+        }
+
+        return t[i][power] = max(case1,case2);
+    }
+    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int k) {
+        unordered_map<int,vector<int>> adj;
+        memset(t,-1,sizeof(t));
+
+        for(auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        return DFS(0,-1,0,adj,coins,k);
+    }
+};

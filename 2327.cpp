5ced5f2f6506852ded1694d3ab8e7f2350f9611d
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-10 19:38:23 +0530

    Update 2327.cpp

--- 2327.cpp
+++ 2327.cpp
@@ -30,3 +30,33 @@ public:
         return total;
     }
 };
+
+
+// BU
+
+class Solution {
+public:
+    const int M = 1e9+7;
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int> t(n+1,0);
+        t[1] = 1;
+        for(int day = 2;day<=n;day++){
+            long long count = 0;
+            for(int prev = day - forget+1;prev<=day-delay;prev++){
+                if(prev > 0){
+                    count = (count + t[prev]%M)%M;
+                }
+                t[day] = count;
+            }
+        }
+
+        int result = 0;
+        for(int day = n-forget+1;day<=n;day++){
+            if(day > 0){
+                result = (result + t[day]%M)%M;
+            }
+        }
+
+        return result;
+    }
+};

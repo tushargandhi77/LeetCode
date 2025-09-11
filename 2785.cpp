d52dbef10131135bef2de13dcf48cd31be585006
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-11 14:13:58 +0530

    Update 2785.cpp

--- 2785.cpp
+++ 2785.cpp
@@ -27,3 +27,40 @@ public:
         return s;
     }
 };
+
+
+// M2 
+
+class Solution {
+public:
+    bool isvowel(char ch){
+        string s = "AEOIUaeoiu";
+        return s.find(ch) != string::npos;
+    }
+    string sortVowels(string s) {
+        int n = s.length();
+        unordered_map<char,int> mp;
+        for(char &ch: s){
+            if(isvowel(ch)){
+                mp[ch]++;
+            }
+        }
+
+        string temp = "AEIOUaeiou";
+
+        int j = 0;
+
+        for(int i = 0;i<n;i++){
+            if(isvowel(s[i])){
+                while(mp[temp[j]] == 0){
+                    j++;
+                }
+
+                s[i] = temp[j];
+                mp[temp[j]]--;
+            }
+        }
+
+        return s;
+    }
+};

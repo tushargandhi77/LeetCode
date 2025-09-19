e094e72722fc15ccc8e4a29f449e3e7b96518891
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-09-19 15:42:07 +0530

    Update 935.cpp

--- 935.cpp
+++ 935.cpp
@@ -39,3 +39,51 @@ public:
         return result;
     }
 };
+
+// BU
+
+class Solution {
+public:
+    unordered_map<int,vector<int>> mp;
+    int M = 1e9+7;
+
+    int knightDialer(int n) {
+        mp[1] = {6,8};
+        mp[2] = {7,9};
+        mp[3] = {4,8};
+        mp[4] = {0,9,3};
+        mp[6] = {0,1,7};
+        mp[7] = {2,6};
+        mp[8] = {1,3};
+        mp[9] = {2,4};
+        mp[0] = {4,6};
+
+        int result = 0;
+
+        vector<vector<int>> t(n,vector<int>(10));
+
+
+        // Base Case
+        for(int cell = 0;cell <= 9;cell++){
+            t[0][cell] = 1;
+        }
+
+        for(int i = 1;i<=n-1;i++){
+            for(int cell = 0;cell<=9;cell++){
+                int ans = 0;
+
+                for(int& nextcell: mp[cell]){
+                    ans = (ans + t[i-1][nextcell]%M)%M;
+                }
+
+                t[i][cell] = ans;
+            }
+        }
+
+        for(int cell = 0;cell<=9;cell++){
+            result = (result + t[n-1][cell]%M)%M; 
+        }
+
+        return result;
+    }
+};

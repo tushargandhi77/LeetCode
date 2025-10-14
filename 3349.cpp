fccb21c51390a6e2c9b44cafbf87c5b0503ba29d
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-10-14 20:26:20 +0530

    Update 3349.cpp

--- 3349.cpp
+++ 3349.cpp
@@ -19,3 +19,34 @@ public:
         return false;
     }
 };
+
+
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int currRun = 1;
+        int prevRun = 0;
+
+        for(int i = 1;i<n;i++){
+            if(nums[i] > nums[i-1]){
+                currRun++;
+            }
+            else{
+                prevRun = currRun;
+                currRun = 1;
+            }
+
+            if(currRun / 2 >= k){
+                return true;
+            }
+            
+            if(min(currRun,prevRun) >= k){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};

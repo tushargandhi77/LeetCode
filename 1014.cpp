c61ea941f4975233d05356d7bf3ccb0872d8bccf
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-10-19 20:18:13 +0530

    Update 1014.cpp

--- 1014.cpp
+++ 1014.cpp
@@ -3,7 +3,6 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n = values.size();
         vector<int> left_max(n,INT_MIN);
-
         int maxi = values[0];
         for(int i = 0;i<n;i++){
             maxi = (values[i] + i) > maxi ? (values[i]+i) : maxi;

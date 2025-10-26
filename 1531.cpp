7e2bd702cc886535132657d6139676827b6872db
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-10-26 21:22:09 +0530

    Update 1531.cpp

--- 1531.cpp
+++ 1531.cpp
@@ -30,3 +30,43 @@ public:
         return solve(0,26,0,k,s);
     }
 };
+
+
+class Solution {
+public:
+    int t[101][101];
+    int solve(int i,int k,string& s){
+        if(k < 0) return 10001;
+
+        if(i >= s.length() || (s.length()-i) <= k) return 0;
+
+        if(t[i][k] != -1) return t[i][k];
+
+        int delete_i = solve(i+1,k-1,s);
+
+        int keep_i = INT_MAX;
+
+        int deleted = 0;
+        int added = 0;
+        int freq = 0;
+        for(int j = i; j < s.length() && deleted <= k;j++){
+            if(s[i] == s[j]){
+                freq++;
+                if(freq == 2 || freq == 10 || freq == 100){
+                    added++;
+                }
+            }
+            else{
+                deleted++;
+            }
+            keep_i = min(keep_i,1 + added + solve(j+1,k-deleted,s));
+        }
+
+        return t[i][k] = min(delete_i,keep_i);
+
+    }
+    int getLengthOfOptimalCompression(string s, int k) {
+        memset(t,-1,sizeof(t));
+        return solve(0,k,s);
+    }
+};

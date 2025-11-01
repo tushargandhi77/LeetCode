665c95d20cd2bf03f5b98b5bd8a053a2d9a52c7e
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-11-01 14:33:04 +0530

    Update 3217.cpp

--- 3217.cpp
+++ 3217.cpp
@@ -12,24 +12,22 @@ class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
         unordered_set<int> st(begin(nums),end(nums));
-
-        while(head != NULL && st.find(head->val) != st.end()){
-            ListNode* temp = head;
+        
+        while(head != NULL && st.count(head->val)){
             head = head->next;
-            delete(temp);
         }
 
         ListNode* curr = head;
+
         while(curr != NULL && curr->next != NULL){
-            if(st.find(curr->next->val) != st.end()){
-                ListNode* temp = curr->next;
+            if(st.count(curr->next->val)){
                 curr->next = curr->next->next;
-                delete(temp);
             }
             else{
                 curr = curr->next;
             }
         }
+
         return head;
     }
 };

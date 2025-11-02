6292fa8a5beb1dec51bb1cc258b1245b63c7264d
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-11-02 15:32:56 +0530

    Update 2257.cpp

--- 2257.cpp
+++ 2257.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
     void fill(int x,int y,int m,int n,vector<vector<int>>& matrix){
-        // right 
-        for(int i = y+1;i<n;i++){
-            if(matrix[x][i] == 0 || matrix[x][i] == 1){
-                matrix[x][i] = 1;
-            }
+        // right
+        for(int j = y+1;j<n;j++){
+            if(matrix[x][j] == 0 || matrix[x][j] == 1) matrix[x][j] = 1;
             else break;
         }
-        // left
-        for(int i = y-1;i>=0;i--){
-            if(matrix[x][i] == 0 || matrix[x][i] == 1){
-                matrix[x][i] = 1;
-            }
+
+        // lleft
+        for(int j = y-1;j>=0;j--){
+            if(matrix[x][j] == 0 || matrix[x][j] == 1) matrix[x][j] = 1;
             else break;
         }
-        // down
+
+        // down 
         for(int i = x+1;i<m;i++){
-            if(matrix[i][y] == 0 || matrix[i][y] == 1){
-                matrix[i][y] = 1;
-            }
+            if(matrix[i][y] == 0 || matrix[i][y] == 1) matrix[i][y] = 1;
             else break;
         }
-        //up
+
+        // up
         for(int i = x-1;i>=0;i--){
-            if(matrix[i][y] == 0 || matrix[i][y] == 1){
-                matrix[i][y] = 1;
-            }
+            if(matrix[i][y] == 0 || matrix[i][y] == 1) matrix[i][y] = 1;
             else break;
         }
+
     }
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
         vector<vector<int>> matrix(m,vector<int>(n,0));
 
-
-        for(auto& v:guards){
-            int a = v[0];
-            int b = v[1];
-            matrix[a][b] = 2;
+        for(auto& guard : guards){
+            int x = guard[0];
+            int y = guard[1];
+            matrix[x][y] = 2;
         }
-        for(auto& v: walls){
-            int a = v[0];
-            int b = v[1];
-            matrix[a][b] = 3;
+        for(auto& wall: walls){
+            int x = wall[0];
+            int y = wall[1];
+            matrix[x][y] = 3;
         }
 
         for(int i = 0;i<m;i++){
@@ -52,14 +47,14 @@ public:
                 }
             }
         }
-        int result =0;
+
+        int result = 0;
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(matrix[i][j] == 0){
-                    result++;
-                }
+                if(matrix[i][j] == 0) result++;
             }
         }
+
         return result;
     }
 };
